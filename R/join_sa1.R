964e65da5ad4840d699f1fb78d5fbffe38ca12ab
Shaun Nielsen <shaunson26@gmail.com>
2022-05-19 09:56:19 +0200

    export join_sa1

--- R/join_sa1.R
+++ R/join_sa1.R
@@ -1,3 +1,10 @@
+#' Join Statistical Area 1 2016 to Mesh Block 2016
+#'
+#' MB 2016 is present in the GANF, we attach SA1 using a concordance dataset \code{mb2016_to_sa12016}
+#'
+#' @param x data.frame, data with MB_2016_CODE
+#'
+#' @export
 join_sa1 <- function(x){
   dplyr::left_join(x, mb2016_to_sa12016, by = c('MB_2016_CODE' = 'MB_CODE_2016'))
 }
